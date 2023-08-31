9601c8c9d362e1751bf169dbbd9e65da3c62496d
Josh Walton <jswalton.dev@gmail.com>
2023-08-31 10:55:49 -0700

    Update init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -325,6 +325,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    path_display = { "smart" },
     mappings = {
       i = {
         ['<C-u>'] = false,
